5396af202f5aeb14d7425c776d00dc9f8c19c2fe
Naoki Shimizu <naoki.shimizu@mercari.com>
2018-03-19 00:23:31 -0700

    3-1: Print Headers

--- 3-1_get_method/main.go
+++ 3-1_get_method/main.go
@@ -22,4 +22,8 @@ func main() {
 	}
 
 	log.Println(string(body))
+	log.Println("Status:", resp.Status)
+	log.Println("StatusCode:", resp.StatusCode)
+	log.Println("Headers:", resp.Header)
+	log.Println("Content-Length:", resp.Header.Get("Content-Length"))
 }
