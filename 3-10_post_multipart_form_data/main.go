ba5c83b973debece6ce3f8ae5c5fcd6fc29297e6
Naoki Shimizu <naoki.shimizu@mercari.com>
2018-03-20 23:54:55 -0700

    3-10: POST method with multipart/form-data

---
+++ 3-10_post_multipart_form_data/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"go/build"
+	"io"
+	"log"
+	"mime/multipart"
+	"net/http"
+	"os"
+
+	"github.com/deme0607/real-world-http-samples/constants"
+)
+
+const filePath = "github.com/deme0607/real-world-http-samples/3-10_post_multipart_form_data/photo.jpg"
+
+func main() {
+	var buffer bytes.Buffer
+
+	writer := multipart.NewWriter(&buffer)
+	writer.WriteField("name", "Naoki Shimizu")
+
+	fileWriter, err := writer.CreateFormFile("thumbnail", "photo.jpg")
+	if err != nil {
+		panic(err)
+	}
+
+	fileName := fmt.Sprintf("%s/src/%s", build.Default.GOPATH, filePath)
+	readFile, err := os.Open(fileName)
+	if err != nil {
+		panic(err)
+	}
+
+	io.Copy(fileWriter, readFile)
+	writer.Close()
+
+	resp, err := http.Post(constants.BaseURL, writer.FormDataContentType(), &buffer)
+	if err != nil {
+		panic(err)
+	}
+
+	log.Println("Status:", resp.Status)
+}
