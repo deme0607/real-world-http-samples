bb93519b89b4826c5c9a68fae12a6415b5fb3899
Naoki Shimizu <naoki.shimizu@mercari.com>
2018-03-24 22:45:22 -0700

    3-12: Use cookie

---
+++ 03-12_use_cookie/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"net/http/cookiejar"
+	"net/http/httputil"
+
+	"github.com/deme0607/real-world-http-samples/constants"
+)
+
+func main() {
+	jar, err := cookiejar.New(nil)
+	if err != nil {
+		panic(err)
+	}
+
+	client := http.Client{
+		Jar: jar,
+	}
+
+	for i := 0; i < 2; i++ {
+		resp, err := client.Get(constants.BaseURL + "cookie")
+		if err != nil {
+			panic(err)
+		}
+
+		dump, err := httputil.DumpResponse(resp, true)
+		if err != nil {
+			panic(err)
+		}
+
+		log.Println(string(dump))
+	}
+}
