38a174e99341f28c3f9939fd743e7554475282ce
Naoki Shimizu <naoki.shimizu@mercari.com>
2018-03-26 22:25:54 -0700

    3-12: Add Cookie handler

--- 01-01_echo_server/main.go
+++ 01-01_echo_server/main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net/http"
 	"net/http/httputil"
+	"strconv"
 
 	"github.com/deme0607/real-world-http-samples/constants"
 )
@@ -20,10 +21,36 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<html><body>hello</body></html>\n")
 }
 
+func cookie(w http.ResponseWriter, r *http.Request) {
+	dump, err := httputil.DumpRequest(r, true)
+	if err != nil {
+		http.Error(w, fmt.Sprint(err), http.StatusInternalServerError)
+		return
+	}
+
+	fmt.Println(string(dump))
+
+	c, err := r.Cookie("count")
+	if err != nil {
+		c = &http.Cookie{
+			Name: "count",
+			Path: "/",
+		}
+	}
+
+	count, _ := strconv.Atoi(c.Value)
+	count++
+	c.Value = strconv.Itoa(count)
+
+	http.SetCookie(w, c)
+	fmt.Fprintf(w, "<html><body>count: %d</body></html>\n", count)
+}
+
 func main() {
 	p := fmt.Sprintf(":%d", constants.Port)
 
 	http.HandleFunc("/", handler)
+	http.HandleFunc("/cookie", cookie)
 
 	log.Println("start http listening ", p)
 	log.Println(http.ListenAndServe(p, nil))
